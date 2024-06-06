fcd2c61dc3087a3df9d22710cd0a37d4f6e2d968
Micr0Byte <26364458+micr0-dev@users.noreply.github.com>
2024-06-06 01:10:55 -0400

    more meta

--- static/script.js
+++ static/script.js
@@ -635,3 +635,12 @@ document.addEventListener('DOMContentLoaded', () => {
     loadSidePlaylists();
     loadMusicList();
 });
+
+function toTitleCase(str) {
+    return str.replace(
+        /\w\S*/g,
+        function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
