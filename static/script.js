64807c73ecaac273b89a06e7742993c4d5ec3e14
Micr0Byte <26364458+micr0-dev@users.noreply.github.com>
2024-06-14 20:35:41 -0400

    trunucate

--- static/script.js
+++ static/script.js
@@ -158,6 +158,31 @@ document.addEventListener('DOMContentLoaded', () => {
         return value;
     }
 
+    function rgbToHsl(r, g, b) {
+        r /= 255, g /= 255, b /= 255;
+
+        const max = Math.max(r, g, b), min = Math.min(r, g, b);
+
+        let h, s, l = (max + min) / 2;
+
+        if (max == min) {
+            h = s = 0;
+        } else {
+            const d = max - min;
+            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+            switch (max) {
+                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                case g: h = (b - r) / d + 2; break;
+                case b: h = (r - g) / d + 4; break;
+            }
+
+            h /= 6;
+        }
+
+        return [h, s, l];
+    }
+
     function checkColorTooDark(color) {
         const c = color.substring(1);      // strip #
         const rgb = parseInt(c, 16);   // convert rrggbb to decimal
